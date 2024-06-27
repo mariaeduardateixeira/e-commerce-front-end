b1e42ba597f5109ace0f542ea51a26c84ac61bc0
ribasmariana <mariana.248.ribas@gmail.com>
2024-06-26 21:16:22 -0300

    css

--- src/components/slider/slider.jsx
+++ src/components/slider/slider.jsx
@@ -1,33 +1,29 @@
 import { FC } from "react";
-import "./slider.css"
+import "./slider.css";
 import Botao from "../Botao/botao";
 import { IProduto } from "./types";
 import image from "./image.jpg";
 
-
 export default function Slider(props) {
 
   const redirecionarDetalhesProduto = (idProduto) => {
-    if(idProduto){
+    if (idProduto) {
       window.location.href = `/produtos/${idProduto}`;
     }
   }
 
   return (
-      <div className="card">
-        <img className="
-        product--image" src={props.url} alt="" />
-        <h2>{props.description}</h2>
-        <p className="price">{props.price}</p>
-        {/* <p>
-          {props.description}
-        </p> */}
-        <Botao
-                label = "Comprar"
-                 onClick = {() => {
-                   redirecionarDetalhesProduto(props.id);
-                 }}
-              />
-      </div>
+    <div className="card">
+      <img className="product--image" src={props.url} alt="" />
+      <h2>{props.name}</h2> {/* Exibindo o nome do produto */}
+      <p className="price">{props.price}</p>
+      {/* <p>{props.description}</p> */}
+      <Botao
+        label="Comprar"
+        onClick={() => {
+          redirecionarDetalhesProduto(props.id);
+        }}
+      />
+    </div>
   );
 }
