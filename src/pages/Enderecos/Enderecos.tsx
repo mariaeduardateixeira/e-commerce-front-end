4045e54a015d5491d1f1784c5a0d3dcf5e4f96fe
Maria Eduarda Teixeira Silvano <mariaeduardat.silvano@gmail.com>
2024-07-03 00:14:53 -0300

    mensagem endereços

--- src/pages/Enderecos/Enderecos.tsx
+++ src/pages/Enderecos/Enderecos.tsx
@@ -1,98 +1,3 @@
-// import React, { useEffect, useState } from 'react';
-// import { apiGet, apiPost, apiPut, apiDelete, STATUS_CODE } from '../../api/RestClient';
-// import { IEndereco } from './types';
-// import EnderecoModal from '../../components/EnderecoModal/EnderecoModal';
-// import { Button, List, ListItem, ListItemText, Typography, IconButton } from '@mui/material';
-// import EditIcon from '@mui/icons-material/Edit';
-// import DeleteIcon from '@mui/icons-material/Delete';
-
-// const Enderecos: React.FC = () => {
-//   const [enderecos, setEnderecos] = useState<IEndereco[]>([]);
-//   const [clienteStore, setClienteStore] = useState<any>(null);
-//   const [isModalOpen, setIsModalOpen] = useState(false);
-//   const [enderecoAtual, setEnderecoAtual] = useState<IEndereco | null>(null);
-
-//   useEffect(() => {
-//     const cliente = JSON.parse(localStorage.getItem("authenticatedUser") || "{}");
-//     if (cliente?.id) {
-//       setClienteStore(cliente);
-//       carregarEnderecos(cliente.id);
-//     } else {
-//       console.log("Cliente não autenticado ou ID não definido");
-//     }
-//   }, []);
-
-//   const carregarEnderecos = async (clienteId: string) => {
-//     const response = await apiGet(`/enderecos/carregarEnderecoByCliente/${clienteId}`);
-//     if (response.status === STATUS_CODE.OK) {
-//       setEnderecos(response.data);
-//     } else {
-//       console.error("Erro ao carregar endereços, status:", response.status);
-//     }
-//   };
-
-//   const abrirModalParaCriar = () => {
-//     setEnderecoAtual(null);
-//     setIsModalOpen(true);
-//   };
-
-//     const abrirModalParaEditar = (endereco: IEndereco) => {
-//     setEnderecoAtual(endereco);
-//      setIsModalOpen(true);
-//      };
-
-//   const fecharModal = () => {
-//     setIsModalOpen(false);
-//   };
-
-//   const salvarEndereco = async (endereco: { rua: string, bairro: string, cidade: string, estado: string }) => {
-//     const enderecoParaSalvar = enderecoAtual ? { ...endereco, id: enderecoAtual.id } : endereco;
-//     const apiEndpoint = enderecoAtual ? `/enderecos/atualizarEndereco/${enderecoAtual.id}` : '/enderecos/criarEndereco';
-  
-    
-//   };
-
-  
-
-//   return (
-//     <div>
-//       <Typography variant="h4" gutterBottom>
-//         Endereços Cadastrados
-//       </Typography>
-//       <Button variant="contained" color="primary" onClick={abrirModalParaCriar}>
-//         Criar Novo Endereço
-//       </Button>
-//       {enderecos.length > 0 ? (
-//         <List>
-//           {enderecos.map((endereco) => (
-//             <ListItem key={endereco.id} secondaryAction={
-//               <>
-                
-               
-//               </>
-//             }>
-//               <ListItemText
-//                 primary={`${endereco.rua}, ${endereco.bairro}`}
-//                 secondary={`${endereco.cidade}, ${endereco.estado}`}
-//               />
-//             </ListItem>
-//           ))}
-//         </List>
-//       ) : (
-//         <Typography variant="subtitle1">Nenhum endereço cadastrado.</Typography>
-//       )}
-//       <EnderecoModal
-//         aberto={isModalOpen}
-//         endereco={enderecoAtual || undefined}
-//         onFechar={fecharModal}
-//         onSalvar={salvarEndereco}
-//       />
-//     </div>
-//   );
-// };
-
-// export default Enderecos;
-// export {};
 
 import React, { useEffect, useState } from 'react';
 import { apiGet, apiPost, apiPut, apiDelete, STATUS_CODE } from '../../api/RestClient';
@@ -135,7 +40,7 @@ const Enderecos: React.FC = () => {
   };
 
   const voltar = () => {
-    navigate('/');
+    navigate('/fecharPedido');
   }
 
   const abrirModalParaEditar = (endereco: IEndereco) => {
@@ -150,6 +55,11 @@ const Enderecos: React.FC = () => {
     const enderecoParaSalvar = enderecoAtual ? { ...endereco, id: enderecoAtual.id } : endereco;
     const apiEndpoint = enderecoAtual ? `/enderecos/atualizarEndereco/${enderecoAtual.id}` : '/enderecos/criarEndereco';
 
+    if (enderecos.length >= 4) {
+      alert("Você já cadastrou o número máximo de endereços (4).");
+            fecharModal();
+            return;
+           }
     try {
       const response = await (enderecoAtual ? apiPut : apiPost)(apiEndpoint, {
         ...enderecoParaSalvar,
